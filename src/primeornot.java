f53a566f6f6bb5b43a82ef74bf6642ba138cc941
kamronraiszadeh <league@WTS10.local>
2018-05-30 16:57:03 -0700

    prime or not

---
+++ src/primeornot.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class primeornot {
+public static void main(String[] args) {
+	String num= JOptionPane.showInputDialog("Enter a number");
+	
+int a=	Integer.parseInt(num);
+
+for (int i = 2; i < a; i++) {
+	
+}
+	
+	if (num%) {
+		
+	}
+}
+}
